fd8961dd50743cbf9f51e5945390c9274ceb79e3
DaNtR3 <danielfdezcilla04@gmail.com>
2025-08-31 17:08:26 -0600

    Feature: 404 page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from src.handlers.year_handler import current_year
 
 app = Flask(__name__)
@@ -12,6 +12,14 @@ def login():
         # Add authentication logic here
         return f"Logged in as {username}"
     return render_template('login.html', custom_css='css/styles_login.css', current_year=current_year())
+
+@app.route('/new-view', methods=['GET'])
+def test():
+    return render_template('home_page.html', custom_css='css/styles_main.css', current_year=current_year())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', custom_css='css/styles_404.css'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
